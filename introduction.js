71d033c44e70117cf2efb7b577742325151fcacc
SofijaSutton <srlsutton@gmail.com>
2020-12-11 16:05:04 -0500

    First Commit to get to GitHub

---
+++ introduction.js
@@ -0,0 +1,5 @@
+function introduceSelf(name, pets) {
+    console.log('Hello! My name is ' + name + ' and I have two ' + pets + ' named Halsey and Coyote-River.');
+}
+
+introduceSelf('Sofija', 'cats');
